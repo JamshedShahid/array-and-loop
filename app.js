5ccf011a823978ea749bfe05b0f9983adff6f2a7
Muhaamd Jamshed <eyes0033@gmail.com>
2021-11-05 07:17:12 +0500

    work done

---
+++ app.js
@@ -0,0 +1,338 @@
+// alert("hello class");
+
+
+// Array
+
+
+// Q:1
+// var stuent =[]
+// stuent[0]="Jamshed"
+// stuent[1]="Umer"
+// stuent[2]="Asad"
+// stuent[3]="raheem"
+// stuent[4]=" Jawad"
+// console.log(stuent)
+
+
+
+// Q:2
+
+// var person = ["John", "Doe", "asad","Faizan" ]
+// console.log(person)
+
+// Q:3
+
+// var str = ["Jamshed" , "shahid" , "umer" , "osama"    ]
+
+// str [5] ="Jawad"
+// console.log(str)
+
+// Q======
+
+// var num1 = [12 ,13 ,14 ,15 ,65]
+// num1 [6] = 67
+// console.log (num1)
+
+// Q===========
+
+// var bol = [true, false]
+// console.log(bol)
+
+// var mix =["jamshed" , true ,23  ,"umer" , false ,65]
+// console.log(mix)
+
+// Q-------
+
+// var Names = ["Jawad" , "Yaqoob" , "Osman"]
+// var num = [300 , 420, 203]
+// document.write(num*100/500)
+
+// Q==========
+
+// var add =["Red" , "Green", "Blue"]
+// console.log(add)
+
+// var add =["Red" , "Green", "Blue"]
+// add.push("yellow" , "lightblue")
+// console.log(add)
+
+
+// Q==========
+
+// var add =["Red" , "Green", "lightblue"]
+
+// console.log(add)
+// var add =["Red" , "Green", "lightblue"]
+// add.push("Blue" ,"orange")
+// console.log(add)
+
+
+
+// Q====
+
+// var ad =["Red" , "Green", "lightblue"]
+
+// console.log(ad)
+// var add =["Red" , "Green", "lightblue"]
+// add.unshift("Blue" ,"orange")
+// console.log(add)
+
+// Q============
+
+// var a =["Red" , "Green", "lightblue"]
+
+// console.log(a)
+// var a =["Red" , "Green", "lightblue"]
+// a.splice(0 ,0,"orange" ,"red" )
+// console.log(a)
+
+
+// Q============
+
+// var b =["Red" , "Green", "lightblue"]
+
+// console.log(b)
+// var b =["Red" , "Green", "lightblue"]
+// b.shift( )
+// console.log(b)
+
+// Q=====
+
+
+// var b =["Red" , "Green", "lightblue"]
+
+// console.log(b)
+// var b =["Red" , "Green", "lightblue"]
+// b.pop( )
+// console.log(b)
+
+// Q====
+// var a =["Red" , "Green", "lightblue"]
+
+// console.log(a)
+// var a =["Red" , "Green", "lightblue"]
+// a.splice(1 ,0,"orange" ,"red" )
+// console.log(a)
+
+
+// Q============
+
+// var a =["Red" , "Green", "lightblue"]
+
+// console.log(a)
+// var a =["Red" , "Green", "lightblue"]
+// a.splice(1 ,2,"orange" ,"red" ,"yellow" ,"gray")
+// console.log(a)
+
+// Q===========
+
+// var ass =[320, 420 , 230 , 50 ,400 ,544]
+// ass.sort(com)
+// console.log(ass)
+
+// function com(a, b){
+//     return a - b;
+// }
+
+
+// Q=====
+
+
+// var  city =["Karachi" , "Punjan" , "Multan" , "Lahore" , "Kpk"]
+// console.log(city)
+
+
+//  var namesArr = [ "Karachi" , "Punjan" , "Multan" , "Lahore" , "Kpk" ]
+
+// var copy = namesArr.slice(1 , 4 )
+
+// console.log(copy)
+
+
+// Q========
+
+
+// var arr = [ "This",  "is",  "my", "cat <br> " ];
+// document.write(arr)
+
+// var arr = [ "This is my cat"];
+// document.write(arr)
+
+
+
+// Q====================
+
+// var mobile=["Apple <br>",
+//     "Samsung <br>", "Motorola <br>", "Nokia <br>", "Sony <br>", " Haier <br>"]
+
+// for(var i=0 ;i<mobile.length;i++){
+//     document.write(mobile [i])
+// }=
+// Q======
+// var num1 =[0 , 1 ,2 ,3]
+// document.write(num1 +"<br>")-
+
+
+// var num2 =[2 , 1 ,0 ,1]
+// document.write(num1 + "<br>")
+
+
+
+
+// var num3 =[  1 ,0 ,1,2]
+// document.write(num2 +"<br>")
+
+
+
+// Q=======
+
+
+
+// for(var i=0;i<=10;i++){
+//     document.write(i +"<br>")
+// }
+
+
+// Q======
+
+
+// var num1 =+prompt("Enter Table")
+// var num2 =+prompt("Enter length")
+// for(var i=1 ;i<=num2;i++){
+//     document.write(num1 +"x" +i+"="+ num1*i+"<br>")
+// }
+
+// Q===========
+
+//  var fruArr = ["Banana" , "Apple" , "Mango" , "Cherry" , "Orange"  ];
+
+// // 
+ 
+
+// for(var  i = 0 ; i < fruArr.length ; i++   ){
+//     document.write(fruArr[ i  ] + "<br>")
+// }
+// var b =fruArr.indexOf( "Banana" )
+// document.write("Index number of Banana is "+ b +"<br>")
+
+// var c =fruArr.indexOf( "Apple")
+// document.write( "Index number of Apple is "+c +"<br>")
+
+// var c =fruArr.indexOf( "Mango")
+// document.write( "Index number of Mango is "+c +"<br>")
+
+
+// var c =fruArr.indexOf( "Cherry")
+// document.write( "Index number of Cherry is "+c +"<br>")
+
+
+// var c =fruArr.indexOf( "Orange")
+// document.write( "Index number of Orange is "+c +"<br>")
+
+
+// Q========
+
+
+// for(var j = 1 ; j <= 15 ; j++ ){
+
+//             // document.write( j + "<br>")
+//             console.log(j)
+// }
+
+
+// for(var m = 1 ; m <= 15 ; m= m+2 ){
+
+//     // document.write( m + "<br>")
+//     console.log(m)
+// }
+
+
+// for(var j = 10 ; j > 0 ; j-- ){
+
+//     // document.write( j + "<br>")
+//     console.log(j)
+// }
+
+
+// for(var j = 2 ; j <= 25 ; j= j+2 ){
+
+//     // document.write( j +  "<br>")
+//     console.log(j)
+// }
+
+// for(var j = 2 ; j <= 25 ; j= j+2 ){
+
+        // document.write( j + "k" +  "<br>")
+        // console.log(j +"k")
+    // }
+// Q=========
+
+
+// var aa=prompt("What do you want")
+//    var a =["cake", "apple", "pie", "cookie", "chips", "patties"]
+//    for(var i=0; i<a.length;i++){
+//        if(a[i] ===a){
+//            alert("order Done")
+           
+           
+//        }
+      
+//    }
+
+//    var isMatch = "no"
+// for(var i = 0 ; i  < a ; i++){
+    
+//     if(aa === a[i]){
+//         alert("WELCOME")
+//         isMatch = "yes"
+//         break
+        
+        
+//     }
+
+// }
+
+// if(isMatch == "no"){
+//     alert("your product is not found")
+// }
+
+// Q=========
+
+
+// var a = [24, 53, 78, 91, 12]
+ 
+   
+
+        
+        
+
+// document.write("The Largest Number is" ,a.sort()[4] )
+
+
+
+// var a = [24, 53, 78, 91, 12]
+ 
+   
+
+        
+        
+
+// document.write("The Smalest Number is" ,a.sort()[a.length-5] )
+
+
+// Q===========
+
+
+// for(var i = 5; i <=100 ; i = i + 5 ){
+
+//          console.log(i)
+//     }
+
+
+// Q====
+// var array=[]
+// console.log(array)
+
+  
+
+
